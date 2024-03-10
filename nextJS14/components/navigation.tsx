3613b1f2d686f39768dcd5e10483713aeef36bc4
Ju-MINJAE <juminjae98@gmail.com>
2024-03-10 22:15:46 +0900

    240310

---
+++ nextJS14/components/navigation.tsx
@@ -0,0 +1,28 @@
+'use client'; // usePathname()을 사용하기 위해 추가
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+
+export default function Navigation() {
+  const path = usePathname(); //현재 경로 나타내는 react hook
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href='/'>Home</Link> {path === '/' ? '🔥' : ''}
+        </li>
+        <li>
+          <Link href='/about-us'>About Us</Link>
+          {path === '/about-us' ? '🔥' : ''}
+        </li>
+        <li>
+          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
