65f832357a7a35ece451dd0dbdbc585549837f1c
Ju-MINJAE <juminjae98@gmail.com>
2024-03-12 15:39:53 +0900

    240312

--- nextJS14/app/(movies)/movies/[id]/page.tsx
+++ nextJS14/app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,24 @@
-export default function MovieDetail({
+import { Suspense } from 'react';
+import MovieInfo from '../../../../components/movie-info';
+import MovieVideos from '../../../../components/movie-videos';
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  return (
+    <div>
+      <h3>Movie Detail Page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <h4>Videos</h4>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
+
+// Suspense : 컴포넌트의 렌더링에 필요한 무언가를 대기할 수 있도록 하는 기능
