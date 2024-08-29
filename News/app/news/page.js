e40633e50d928e8089f9171cf2654e2b4da72c6e
Ju-MINJAE <juminjae98@gmail.com>
2024-08-29 10:38:43 +0900

    dummy 데이터 추가

--- News/app/news/page.js
+++ News/app/news/page.js
@@ -1,19 +1,23 @@
 import Link from 'next/link';
 
+import { DUMMY_NEWS } from '@/\bdummy-news';
+
 export default function NewsPage() {
   return (
     <>
       <h1>New Page</h1>
-      <ul>
-        <li>
-          <Link href="/news/a">First Link Item</Link>
-        </li>
-        <li>
-          <Link href="/news/b">Second Link Item</Link>
-        </li>
-        <li>
-          <Link href="/news/c">Third Link Item</Link>
-        </li>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
